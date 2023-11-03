b2b577c843a29e42bfda588c1e58dafde49727fc
Nathan Nette <nathan.nette@gmail.com>
2023-11-03 23:13:11 +1100

    Set up interface working with buttons and created script for getting the date.

---
+++ UnityFiles/HobbyTracker/Assets/Scripts/Date.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Date : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Gets Date and Allocates the text on top for it
+    // and sets the correct buttons
+    void GetDate()
+    {
+        float fDate = 0;
+        
+       
+    }
+}
