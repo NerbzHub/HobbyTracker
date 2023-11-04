82b9293b02ecd46972ef62565050408572fa8625
Nathan Nette <nathan.nette@gmail.com>
2023-11-04 22:00:54 +1100

    Can get text and displaying in console, Just need to allocate it to the text in UI.

--- UnityFiles/HobbyTracker/Assets/Scripts/Date.cs
+++ UnityFiles/HobbyTracker/Assets/Scripts/Date.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Date : MonoBehaviour
 {
+
+    public TMPro.Text uiText = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //var displayDate = 
+
+        string date = DateTime.Now.ToString("mm:dd:yyyy");
+
+        date = DateTime.Now.ToString("dd/MM/yyyy");
+
+        Debug.Log(date);
+
+
+        //displayDate.text = date;
+    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Gets Date and Allocates the text on top for it
     // and sets the correct buttons
     void GetDate()
     {
-        float fDate = 0;
         
-       
+
     }
 }
